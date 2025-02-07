45d16f55d0495e5a6e9e2a91103fcc4d4c128687
psrok1 <pawel.srokosz@cert.pl>
2025-02-07 12:49:50 +0100

    Keep support for Py3.8

--- lint_python/constants.py
+++ lint_python/constants.py
@@ -1,2 +1,2 @@
 __version__ = "2.3.0"
-USED_LINTERS = ["isort==5.13.2", "black==25.1.0", "flake8==7.1.1", "mypy==1.15.0"]
+USED_LINTERS = ["isort==5.13.2", "black==24.8.0", "flake8==7.1.1", "mypy==1.14.1"]
